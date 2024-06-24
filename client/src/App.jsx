7ce5ad5278363c2edaba4866d94e0a3b77820306
Colson <colsoncalifornication@gmail.com>
2024-06-25 00:15:42 +0545

    build post submission: create post create component + add bootstrap cdn + implement two way property binding on input form + send created post to Post microservices backend when form is submitted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 const App = () => {
-  return <div style={{ color: 'dodgerblue', fontSize: '3rem' }}>Tweet app</div>;
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
 
 export default App;
