84fe93a733235972a64d4618cd0021be6545fb8c
Colson <colsoncalifornication@gmail.com>
2024-06-25 22:25:09 +0545

    pass incoming events: save post on PostCreated event and comment with the associated post on ComentCreated event in the Query service i.e Query service will now has all the data that is required to get the entire list of posts and all the associated comments

--- query/index.js
+++ query/index.js
@@ -6,9 +6,41 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+// @ posts in-memory data structure
+/* posts === {
+    a1b2c3: {
+      id: 'a1b2c3',
+      titile: 'post title',
+      comments: [{ id: 'a1z9', content: 'comment!' }],
+    },
+  }; */
 
-app.post('/events', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
