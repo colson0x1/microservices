117642ab05b6cb961d47353e9e444a34ac2b9173
Colson <colsoncalifornication@gmail.com>
2024-07-11 16:09:13 +0545

    update event-bus code so that it once again sends events to newly created Comments, Query and Moderation ClusterIP Services + rebuild that image + push it up to dockerhub + update the deployment + test if all microservices are loaded into the Cluster or not

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,10 +12,9 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios
-    // .post('http://localhost:4000/events', event)
-    .post('http://posts-clusterip-srv:4000/events', event)
-    .catch((err) => console.log(err.message));
+  // axios
+  //   .post('http://localhost:4000/events', event)
+  //   .catch((err) => console.log(err.message));
   // axios
   //   .post('http://localhost:4001/events', event)
   //   .catch((err) => console.log(err.message));
@@ -25,6 +24,19 @@ app.post('/events', (req, res) => {
   // axios
   //   .post('http://localhost:4003/events', event)
   //   .catch((err) => console.log(err.message));
+
+  axios
+    .post('http://posts-clusterip-srv:4000/events', event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post('http://comments-srv:4001/events', event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post('http://query-srv:4002/events', event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post('http://moderation-srv:4003/events', event)
+    .catch((err) => console.log(err.message));
 
   res.send({ status: 'OK' });
 });
